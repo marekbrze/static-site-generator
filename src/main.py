3c8979da6e0b94370fdc08b0e7208c5b5a8bf504
Marek Brzeziński <marek.j.brzezinski@gmail.com>
2025-03-16 15:41:02 +0100

    TextNode lesson ready

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
-print("hello world")
+from textnode import TextNode
 
+
+def main():
+    text_object = TextNode("Testowy", "NORMAL", "https://example.com")
+    text_object2 = TextNode("Testowy", "NORMAL", "https://example.com")
+    print(text_object, text_object2)
+    print(text_object.__eq__(text_object2))
+
+
+main()
