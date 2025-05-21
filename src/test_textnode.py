352ba55d40c4a2f3911b50761a02283f51198fa6
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-05-21 22:23:17 +0200

    working on text tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -37,6 +37,31 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "strong")
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_text_to_html_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_to_html_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_to_html_link(self):
+        node = TextNode("This is a text node", TextType.LINK, url="http://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.url, "http://example.com")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_to_html_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "strong")
+        self.assertEqual(html_node.value, "This is a text node")
+
 
 if __name__ == "__main__":
     unittest.main()
