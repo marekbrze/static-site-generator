53b80196b6901513f3a75303e5a1ed2bdfd893bd
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-06-19 23:04:47 +0200

    Further work

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,3 +1,8 @@
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
+    for node in old_nodes:
+        node_nodes = []
+        for character in node:
+
+        new_nodes.append(node_nodes)
     return new_nodes
